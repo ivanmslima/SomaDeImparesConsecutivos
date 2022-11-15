f1a92e7fb4bc22f993f9d1bd5f5a1035c88d486c
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-15 08:39:02 -0300

    Adicionar arquivos de projeto.

---
+++ SomaDeImparesConsecutivos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int x, y, min, max, soma;
+            Console.WriteLine("Digite 2 números para a soma de impares consecutivos:");
+
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+
+            soma = 0;
+            for (int i=min+1; i<max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma);
+
+            Console.ReadLine();
+
+        }
+    }
+}
